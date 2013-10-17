8a95ef042d70b25b1163859eedd68bc59215c6fd
habibi <guilvee@gmail.com>
2013-10-17 09:12:45 +0800

    Init 2

---
+++ CopySDK/CopySDK/CopyConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopySDK
+{
+    public class CopyConfig
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+
+        
+    }
+}
