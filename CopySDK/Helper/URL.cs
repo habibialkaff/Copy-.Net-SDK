8c10e25ac7b490b0651f0a0f8a0968af34542150
Habibi <guilvee@gmail.com>
2013-10-17 18:28:55 +0800

    Init 3

---
+++ CopySDK/Helper/URL.cs
@@ -0,0 +1,10 @@
+﻿namespace CopySDK.Helper
+{
+    public static class URL
+    {
+        public const string RequestToken = "https://api.copy.com/oauth/request";
+        public const string Authorize = "https://www.copy.com/applications/authorize";
+        public const string AccessToken = "https://api.copy.com/oauth/access";
+        public const string RESTRoot = "https://api.copy.com/rest/";
+    }
+}
